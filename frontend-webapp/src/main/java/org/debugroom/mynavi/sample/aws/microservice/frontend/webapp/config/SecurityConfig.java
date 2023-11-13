b0b4f7eb733e420f15e5741c25bdb99918c7d14b
Ran Wakabayashi <lululira.k@gmail.com>
2023-11-13 22:29:28 +0900

    Spring Securityバージョンアップに伴い修正

--- frontend-webapp/src/main/java/org/debugroom/mynavi/sample/aws/microservice/frontend/webapp/config/SecurityConfig.java
+++ frontend-webapp/src/main/java/org/debugroom/mynavi/sample/aws/microservice/frontend/webapp/config/SecurityConfig.java
@@ -2,82 +2,65 @@ package org.debugroom.mynavi.sample.aws.microservice.frontend.webapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-
-import org.debugroom.mynavi.sample.aws.microservice.frontend.webapp.app.web.security.CustomUserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 import org.debugroom.mynavi.sample.aws.microservice.frontend.webapp.app.web.security.LoginSuccessHandler;
 import org.debugroom.mynavi.sample.aws.microservice.frontend.webapp.app.web.security.SessionExpiredDetectingLoginUrlAuthenticationEntryPoint;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	@Lazy
+	PasswordEncoder passwordEncoder;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/**");
-    }
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring().requestMatchers("/static/**");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/favicon.ico").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/static/**").permitAll()
-                .antMatchers("/timeout").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .formLogin()
-                .loginProcessingUrl("/authenticate")
-                .loginPage("/login")
-                .successHandler(loginSuccessHandler())
-                .failureUrl("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .permitAll()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint())
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .permitAll();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				authz -> authz.requestMatchers("/favicon.ico").permitAll()
+							.requestMatchers("/webjars/**").permitAll()
+							.requestMatchers("/static/**").permitAll()
+							.requestMatchers("/timeout").permitAll()
+							.anyRequest().authenticated())
+		.csrf(csrf -> csrf.disable())
+		.formLogin(login -> login.loginProcessingUrl("/authenticate")
+								.loginPage("/login")
+								.successHandler(loginSuccessHandler())
+								.failureUrl("/login")
+								.usernameParameter("username")
+								.passwordParameter("password").permitAll())
+		.exceptionHandling(eh -> eh.authenticationEntryPoint(authenticationEntryPoint()))
+		.logout(logout -> logout.logoutSuccessUrl("/login"));
+		return http.build();
+	}
 
-    @Bean
-    public LoginSuccessHandler loginSuccessHandler(){
-        return new LoginSuccessHandler();
-    }
+	@Bean
+	public LoginSuccessHandler loginSuccessHandler() {
+		return new LoginSuccessHandler();
+	}
 
-    @Bean
-    AuthenticationEntryPoint authenticationEntryPoint() {
-        return new SessionExpiredDetectingLoginUrlAuthenticationEntryPoint("/login");
-    }
+	@Bean
+	AuthenticationEntryPoint authenticationEntryPoint() {
+		return new SessionExpiredDetectingLoginUrlAuthenticationEntryPoint("/login");
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService())
-                .passwordEncoder(passwordEncoder);
-    }
 }
