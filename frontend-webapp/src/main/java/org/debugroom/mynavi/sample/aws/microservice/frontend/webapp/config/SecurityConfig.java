317e6ef80b331324d75004fdde9d51a5dcdefb96
Ran Wakabayashi <lululira.k@gmail.com>
2023-11-13 22:39:02 +0900

    不要なimport文削除

--- frontend-webapp/src/main/java/org/debugroom/mynavi/sample/aws/microservice/frontend/webapp/config/SecurityConfig.java
+++ frontend-webapp/src/main/java/org/debugroom/mynavi/sample/aws/microservice/frontend/webapp/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -19,48 +18,50 @@ import org.debugroom.mynavi.sample.aws.microservice.frontend.webapp.app.web.secu
 @Configuration
 public class SecurityConfig {
 
-	@Autowired
-	@Lazy
-	PasswordEncoder passwordEncoder;
+    @Autowired
+    @Lazy
+    PasswordEncoder passwordEncoder;
 
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring().requestMatchers("/static/**");
-	}
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/static/**");
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-				authz -> authz.requestMatchers("/favicon.ico").permitAll()
-							.requestMatchers("/webjars/**").permitAll()
-							.requestMatchers("/static/**").permitAll()
-							.requestMatchers("/timeout").permitAll()
-							.anyRequest().authenticated())
-		.csrf(csrf -> csrf.disable())
-		.formLogin(login -> login.loginProcessingUrl("/authenticate")
-								.loginPage("/login")
-								.successHandler(loginSuccessHandler())
-								.failureUrl("/login")
-								.usernameParameter("username")
-								.passwordParameter("password").permitAll())
-		.exceptionHandling(eh -> eh.authenticationEntryPoint(authenticationEntryPoint()))
-		.logout(logout -> logout.logoutSuccessUrl("/login"));
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests(
+                authz -> authz.requestMatchers("/favicon.ico").permitAll()
+                            .requestMatchers("/webjars/**").permitAll()
+                            .requestMatchers("/static/**").permitAll()
+                            .requestMatchers("/timeout").permitAll()
+                            .anyRequest().authenticated())
+        .csrf(csrf -> csrf.disable())
+        .formLogin(
+                login -> login.loginProcessingUrl("/authenticate")
+                                .loginPage("/login")
+                                .successHandler(loginSuccessHandler())
+                                .failureUrl("/login")
+                                .usernameParameter("username")
+                                .passwordParameter("password").permitAll())
+        .exceptionHandling(eh -> eh.authenticationEntryPoint(authenticationEntryPoint()))
+        .logout(logout -> logout.logoutSuccessUrl("/login"));
+        return http.build();
+    }
 
-	@Bean
-	public LoginSuccessHandler loginSuccessHandler() {
-		return new LoginSuccessHandler();
-	}
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler();
+    }
 
-	@Bean
-	AuthenticationEntryPoint authenticationEntryPoint() {
-		return new SessionExpiredDetectingLoginUrlAuthenticationEntryPoint("/login");
-	}
+    @Bean
+    AuthenticationEntryPoint authenticationEntryPoint() {
+        return new SessionExpiredDetectingLoginUrlAuthenticationEntryPoint("/login");
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
